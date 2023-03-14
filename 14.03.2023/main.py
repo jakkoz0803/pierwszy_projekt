c8dba970a1448f2a76bb5b50dcd1fa20d65c0e03
jakkoz0803 <115089022+jakkoz0803@users.noreply.github.com>
2023-03-14 15:25:11 +0100

    Add files via upload

---
+++ 14.03.2023/main.py
@@ -0,0 +1,24 @@
+import math
+
+print("-------------- zad 1 --------------")
+print(math.pow(math.e,10))
+print(math.pow(math.log((5+math.pow(math.sin(8),2)),math.e),(1/6)))
+print(math.floor(3.55))
+print(math.ceil(4.80))
+
+print("-------------- zad 2 --------------")
+imie = "JAKUB"
+nazwisko = "KOZICKI"
+print(imie.capitalize(), nazwisko.capitalize())
+
+print("-------------- zad 3 --------------")
+tekst_piosenki = "The ting goes skrraa POP POP KA KA KA"
+print(tekst_piosenki)
+print(tekst_piosenki.count("KA"))
+
+print("-------------- zad 4 --------------")
+dowolna_zmienna_lancuchowa = "losowe slowa ktore przychodza mi do glowy"
+print(dowolna_zmienna_lancuchowa)
+print(dowolna_zmienna_lancuchowa[1], dowolna_zmienna_lancuchowa[len(dowolna_zmienna_lancuchowa) - 1])
+
+print("-------------- zad 5 --------------")
