de6487f0af8fd37382506a8d146b2b7ec4228b42
jakkoz0803 <115089022+jakkoz0803@users.noreply.github.com>
2023-03-15 11:30:50 +0100

    Add files via upload

--- 15.03.2023/main.py
+++ 15.03.2023/main.py
@@ -1,3 +1,5 @@
+import random
+
 print("podaj a: ")
 a = input()
 print("podaj b: ")
@@ -15,3 +17,20 @@ lista_b = [4**x for x in range(7)]
 print(lista_b)
 lista_c = [x for x in lista_b if x % 2 == 0]
 print(lista_c)
+
+print("============ zad 2 ============")
+listy1 = []
+for i in range(10):
+    listy1.append(random.randint(1,10))
+print(listy1)
+listy1_parzyste = [x for x in listy1 if x % 2 == 0]
+print(listy1_parzyste)
+
+print("============ zad 3 ============")
+# Utwórz słownik z produktami spożywczymi do kupienia.
+# Klucz to niech będzie nazwa produktu a
+# wartość - jednostka w jakiej się je kupuje
+# (np. sztuki, kg). Wykorzystaj Python Comprehension do
+# zdefiniowania nowej listy, gdzie będą produkty,
+# których wartości to sztuki.
+
