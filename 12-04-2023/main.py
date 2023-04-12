db3feb36ecda4e67f555f6b4c5e49ddd3a3470c0
jakkoz0803 <115089022+jakkoz0803@users.noreply.github.com>
2023-04-12 12:25:43 +0200

    Add files via upload

--- 12-04-2023/main.py
+++ 12-04-2023/main.py
@@ -72,3 +72,51 @@ print(mat_diag_k)
 #Numpy jest w stanie stworzyc tablice jednowymiarowa z cos tam
 z = np.fromiter(range(5), dtype='int32')
 print(z)
+#
+znaki = b'ogolna'
+z1 = np.frombuffer(znaki,dtype='S1') # string dlugosci 1
+print(z1)
+z2 = np.frombuffer(znaki,dtype='S2') # string dlugosci 2
+print(z2)
+#
+znaki = 'ogolna'
+z3 = np.array(list(znaki))
+z4 = np.array(list(znaki), dtype='S1')
+z5 = np.array(list(b'ogolna'))
+z6 = np.fromiter(znaki,dtype = 'S1')
+print("===================")
+print(z3)
+print(z4)
+print(z5)
+print(z6)
+print("===================")
+#
+a = np.arange(10)
+print(a)
+s = slice(2,7,2)
+print(a[s])
+#mozemy ciac tablice rowniez w sposob znany z ciecia cos tam
+print(a[2:7:2])
+#lub tak
+print(a[1:])
+print(a[2:5])
+# w podobny sposob postepujemy w przypadki tablic wielocos tam
+mat = np.arange(25)
+#teraz zmieniamy ksztalt tablicy jednowymiarowej na mcos tam
+mat = mat.reshape((5,5))
+print(mat)
+print(mat[1:])              # od drugiego wiersza
+print(mat[:,1])             # druga kolumna jako wektor
+print(mat[:,1:2])           # druga kolumna jako wektor ale inaczej, z macierzy 5x5 -> 5x1, ostatni element cos tam
+print(mat[:,-1])            # ostatnia kolumna
+print(mat[:,4:5])           # ostatnia kolumna to samo co druga kolumna
+print(mat[2:6,1:3])         # 2 i 3 kolumna dla 3,4,5 wierszy
+print(mat[:, range(2,6,2)]) # 3 i 5 kolumna (sam dwukropek - wszystkie wiersze)
+print(mat[:, [2,4]])        # to samo ale inaczej
+
+x = np.array([[0,1,2],[3,4,5],[6,7,8],[9,10,11]])
+print(x)
+rows = np.array([[0,0],[3,3]])
+cols = np.array([[0,2],[0,2]])
+y = x[rows,cols]
+print(y)
